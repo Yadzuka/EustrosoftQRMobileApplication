7b0ade2a1e3821a4b78f377b7fbb612eb955886e
Yadzuka <yadzuka@eustrosoft.org>
2021-12-25 22:09:55 +0300

    Fragment was renamed as well as naming of this part of application

---
+++ app/src/main/java/ru/eustrosoft/androidqr/ui/qrencoder/QREncoderFragment.java
@@ -0,0 +1,46 @@
+package ru.eustrosoft.androidqr.ui.qrencoder;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.zxing.WriterException;
+
+import ru.eustrosoft.androidqr.R;
+import ru.eustrosoft.androidqr.util.qr.QREncoder;
+
+public class QREncoderFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_qrencoder, container, false);
+
+        EditText qrText = view.findViewById(R.id.qr_text);
+        ImageView qrImage = view.findViewById(R.id.qr_image_view);
+        Button encodeButton = view.findViewById(R.id.qr_encode_button);
+        QREncoder encoder = new QREncoder();
+
+        encodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // Set russian symbols support
+                    qrImage.setImageBitmap(encoder.createQRImage(1024, qrText.getText().toString()));
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return view;
+    }
+}
